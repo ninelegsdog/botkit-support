c3c4bbd95107eb7192db41d46a3c0c3e59e041ce
ninelegsdog <ninelegsdog@gmail.com>
2026-09-25 16:18:25 +0000

    feat(redis): namespace bot FSM and throttle keys

--- src/core/bot_factory.py
+++ src/core/bot_factory.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.base import DefaultKeyBuilder
 from aiogram.fsm.storage.redis import RedisStorage
 
 from src.core.config import Config
@@ -17,7 +18,7 @@ class AppState:
         self.storage = Storage(self.db)
         self.metrics = Metrics()
         self.bot = Bot(token=config.bot_token, default=DefaultBotProperties(parse_mode="HTML"))
-        self.fsm_storage = RedisStorage.from_url(config.redis_url)
+        self.fsm_storage = RedisStorage.from_url(config.redis_url, key_builder=DefaultKeyBuilder(prefix="fsm:support"))
         self.dp = Dispatcher(storage=self.fsm_storage)
 
 
